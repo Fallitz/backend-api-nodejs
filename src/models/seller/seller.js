5865feb628559c6a8bb8e95fe7f21476c9c0ab1f
Fallitz <47279161+Fallitz@users.noreply.github.com>
2021-12-29 15:54:45 -0300

    Update seller.js

--- src/models/seller/seller.js
+++ src/models/seller/seller.js
@@ -17,18 +17,10 @@ class seller extends Model{
                                         return {status: false, message: 'Usuário já possui loja cadastrada'};
                                 }else{
                                         const ownerId = sellerWasRegistered[0].id;
-                                        const country = data.address.country;
-                                        const state = data.address.state;
-                                        const city = data.address.city;
-                                        const district = data.address.district;
-                                        const street = data.address.street;
-                                        const number = data.address.number;
-                                        const complement = data.address.complement;
-                                        const reference = data.address.reference;
-                                        const zipCode = data.address.zipCode;
+                                        const address = data.address;
                                         delete data.address;
 
-                                        const seller = await knex('sellers').insert({...data, id, ownerId, country, state, city, district, street, number, complement, reference, zipCode}).then(() => {return knex ('sellers').where('id', id).select('name')});
+                                        const seller = await knex('sellers').insert({...data, ...address, id, ownerId}).then(() => {return knex ('sellers').where('id', id).select('name')});
                                         if(seller.length > 0){
                                                 return {status: true, data: seller[0]};
                                         }else{
