635f8b6d76b712b273b3004795cca9a97ac62749
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-05 18:16:14 -0300

    mongodb add

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,21 +1,62 @@
+require("dotenv").config();
 const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const signale = require('signale');
 
+/*
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGODB_URI;
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'myProject';
+const dbName = process.env.MONGODB_DBNAME;*/
 
-async function main() {
+class MongoDb {
+
+    constructor() { }
+
+    async connect() {
+        try {
+            
+            signale.info('conectando ao MongoDb...');
+            const mongodbUrl = process.env.MONGODB_URI || 'ENV VAR MONGODB_URL IS NOT DEFINED';
+            signale.info('MONGODB_URL: ' + mongodbUrl);
+    
+            const mongoClient = await MongoClient.connect(mongodbUrl, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+    
+            signale.info(`MONGODB_DATABASE ${process.env.MONGODB_URI || 'UNDEFINED'}`)
+            const Db = mongoClient.db(process.env.MONGODB_URI)
+    
+            signale.info('conectado ao mongoDb');
+    
+            return Db;
+
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
+    static async getDb() {
+        if (dbInstance) {
+            return dbInstance;
+        } else {
+            const db = await new MongoDb().connect();
+            dbInstance = db;
+            return db;
+        }
+    }
+}
+
+/**
+
+async function main(collection) {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
-  
-  const collection = db.collection('collection');
+
+  const collection = db.collection(collection);
 
   // the following code examples can be pasted here...
 
@@ -26,7 +67,24 @@ main()
   .then(console.log)
   .catch(console.error)
   .finally(() => client.close());
+*/
 
+module.exports ={
+    collections: {
+        usuario: 'usuario',
+        place: 'place',
+        municipio: 'municipio',
+        produto: 'produto',
+        funcionario: 'funcionario',
+        analytics: 'analytics',
+        carrinho: 'carrinho',
+        pedido: 'pedido',
+        produtoCategoria: 'produtoCategoria',
+        banner: 'banner',
+        analytic: 'analytic',
+        visitante: 'visitante',
+    },
+} 
 
-module.exports = main();
+module.exports = MongoDb;
 
