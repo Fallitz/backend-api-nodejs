f335771bb5c01cf4e5fb065cc19064eefa0cead7
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-09 13:07:38 -0300

    Update productsController.js

--- src/modules/product/controllers/productsController.js
+++ src/modules/product/controllers/productsController.js
@@ -6,7 +6,22 @@ const util = require('../../../repositories/util/util');
 module.exports = {
 
     async create(req, res){
-
+        const data = req.body;
+        sellerValidator.create.validate({...data}).then(async function () {
+            try {
+                const seller = new Seller();
+                const sellerRegistered = await seller.create({...data, ownerId: req.tokenData.id});
+                if(sellerRegistered.status){
+                    return res.status(201).json({status: true, message: 'Loja criada com sucesso', data: {store: sellerRegistered.data}});
+                }else{
+                    return res.status(403).json({status: false, message: sellerRegistered.message, field: sellerRegistered.field});
+                }             
+            } catch (error) {
+                res.status(500).json({status: false, message: error.message});
+            }
+        }).catch(function (err) {
+            res.status(500).json({status: false, message: err.errors[0], field: err.path});
+        });
     },
 
 }
