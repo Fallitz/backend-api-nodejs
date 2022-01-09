d1bafb50281f98f806e397dec4e78cff421508ea
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-09 13:07:43 -0300

    Create product.js

---
+++ src/repositories/http/validators/product.js
@@ -0,0 +1,35 @@
+const Validator = require('../Validator')
+
+const schema ={
+    create: Validator.object().shape({
+        name: Validator.string().required().min(3),
+        description: Validator.string(),
+        phone: Validator.string().required().min(13).max(14),
+        address: Validator.object().required().shape({
+            country: Validator.string().required().min(3),
+            state: Validator.string().required().min(3),
+            city: Validator.string().required().min(3),
+            district: Validator.string().required().min(3),
+            street: Validator.string().required().min(3),
+            number: Validator.string().required().min(1),
+            complement: Validator.string(),
+            reference: Validator.string(),
+            zipCode: Validator.string().required().min(8).max(8)	
+        }), 
+        nationalRegister: Validator.string().required().min(14).max(14),
+        socialReason: Validator.string().required(3),
+        type: Validator.string().required(),
+        category: Validator.string().required(),
+        acceptDeliver: Validator.boolean().required(),
+        acceptWithdrawal: Validator.boolean().required(),
+        avatar: Validator.string(),
+    }),
+    id: Validator.object().shape({
+        id: Validator.string().required().min(36).max(36),
+    }),
+    search: Validator.object().shape({
+        search: Validator.string().required().min(3)
+    }),
+} 
+
+module.exports = schema
