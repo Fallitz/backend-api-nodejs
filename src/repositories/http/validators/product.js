b3ea9e87593a0a8a35f2b9466507ed1ed5eca903
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-11 17:56:36 -0300

    Update product.js

--- src/repositories/http/validators/product.js
+++ src/repositories/http/validators/product.js
@@ -4,24 +4,8 @@ const schema ={
     create: Validator.object().shape({
         name: Validator.string().required().min(3),
         description: Validator.string(),
-        phone: Validator.string().required().min(13).max(14),
-        address: Validator.object().required().shape({
-            country: Validator.string().required().min(3),
-            state: Validator.string().required().min(3),
-            city: Validator.string().required().min(3),
-            district: Validator.string().required().min(3),
-            street: Validator.string().required().min(3),
-            number: Validator.string().required().min(1),
-            complement: Validator.string(),
-            reference: Validator.string(),
-            zipCode: Validator.string().required().min(8).max(8)	
-        }), 
-        nationalRegister: Validator.string().required().min(14).max(14),
-        socialReason: Validator.string().required(3),
         type: Validator.string().required(),
         category: Validator.string().required(),
-        acceptDeliver: Validator.boolean().required(),
-        acceptWithdrawal: Validator.boolean().required(),
         avatar: Validator.string(),
     }),
     id: Validator.object().shape({
