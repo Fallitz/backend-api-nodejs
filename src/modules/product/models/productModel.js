70a7f27785d2dfaebc857f67c87ceba43179ea0a
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-12 21:41:47 -0300

    Update productModel.js

--- src/modules/product/models/productModel.js
+++ src/modules/product/models/productModel.js
@@ -2,3 +2,6 @@ const knex = require('../../../config/database');
 const Model = require('../../../repositories/models/model');
 const util = require('../../../repositories/util/util');
 
+class Products extends Model{
+    
+}
