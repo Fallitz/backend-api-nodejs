89b0faf486728e875dcb203ca9227fd444f5067d
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-14 22:25:24 -0300

    Update 20220104021158_products_table_migration.js

--- src/config/migrations/20220104021158_products_table_migration.js
+++ src/config/migrations/20220104021158_products_table_migration.js
@@ -5,10 +5,12 @@ exports.up = function(knex) {
         table.uuid('id').primary().defaultTo(knex.raw("(UUID())"));
         table.uuid('sellerId').references('id').inTable('sellers').notNullable().defaultTo(knex.raw("(UUID())"));
         table.string('name', 100).notNullable();
-        table.string('description');
+        table.string('description', 500).notNullable();
         table.string('category', 100).notNullable();
         table.string('subcategory', 100).notNullable();
         table.string('pryce', 20).notNullable();
+        table.string('pryceAPrazo', 20).notNullable();
+        table.string('pryceAVista', 20).notNullable();
         table.mediumtext('avatar').notNullable();
         table.float('rating');
         table.boolean('active').defaultTo('true').notNullable();
