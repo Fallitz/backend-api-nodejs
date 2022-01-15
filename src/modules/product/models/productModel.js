74db8a883d9c1ff04e7c7ba2cafc8a42749af36e
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-15 17:50:14 -0300

    Update productModel.js

--- src/modules/product/models/productModel.js
+++ src/modules/product/models/productModel.js
@@ -6,21 +6,21 @@ const { v4: uuidv4 } = require('uuid');
 class Products extends Model{
     async create(data){
         try {
-                const sellerWasRegistered = await knex('sellers').where('ownerId', data.ownerId).select('id');
-                if(sellerWasRegistered.length > 0){
-                        const id = uuidv4();
-                        delete data.ownerId;
-                        const product = await knex('products').insert({...data, id, sellerId: sellerWasRegistered[0].id}).then(() => {return knex ('products').where('id', id).select('name')});
-                        if(product.length > 0){
-                                return {status: true, data: product[0]};
-                        }else{
-                                return {status: false, message: 'Produto não foi cadastrado'};
-                        }
-                }else{
-                        return {status: false, message: 'Loja não cadastrada', field: 'ownerId'};
-                }
+			const sellerWasRegistered = await knex('sellers').where('ownerId', data.ownerId).select('id');
+			if(sellerWasRegistered.length > 0){
+					const id = uuidv4();
+					delete data.ownerId;
+					const product = await knex('products').insert({...data, id, sellerId: sellerWasRegistered[0].id}).then(() => {return knex ('products').where('id', id).select('name')});
+					if(product.length > 0){
+						return {status: true, data: product[0]};
+					}else{
+						return {status: false, message: 'Produto não foi cadastrado'};
+					}
+			}else{
+				return {status: false, message: 'Loja não cadastrada', field: 'ownerId'};
+			}
         }catch (error) {
-                return {status: false, message: error.sqlMessage ?? error.message};
+			return {status: false, message: error.sqlMessage ?? error.message};
         }
     }
 }
