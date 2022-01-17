7630baf64a23d1dca9fc184232cc9158ed524e49
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-17 19:32:00 -0300

    package update

--- src/modules/product/routers/productRouters.js
+++ src/modules/product/routers/productRouters.js
@@ -8,7 +8,7 @@ class userRouter {
 
         const Router = express.Router();
     
-        Router.post('/', AuthenticateToken ,ProductController.create);                                                                  //CREATE PRODUCT
+        Router.post('/create', AuthenticateToken ,ProductController.create);                                                                  //CREATE PRODUCT
         Router.get('/:id', AuthenticateToken ,ProductController.getById);                                                               //GET PRODUCT BY ID
         Router.get('/seller/:id', AuthenticateToken ,ProductController.getBySellerId);                                                  //GET PRODUCT BY SELLER ID
 
