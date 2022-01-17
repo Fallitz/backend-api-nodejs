5f95579704b879ee62c17afae745ef0c95bd05a1
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-16 22:21:50 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -6,9 +6,10 @@ module.exports = {
     client: 'mysql',
     connection: {
       host : process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user : process.env.DB_USER,
       password : process.env.DB_PASS,
-      database : process.env.DB_NAME
+      database : process.env.DB_NAME,
     },
     migrations: {
       tableName: 'knex_migrations',
@@ -22,6 +23,7 @@ module.exports = {
     client: 'mysql',
     connection: {
       host : process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user : process.env.DB_USER,
       password : process.env.DB_PASS,
       database : process.env.DB_NAME
