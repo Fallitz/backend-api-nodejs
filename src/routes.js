1ff91acc84eec03349e7872aa567bd16cf804812
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-25 21:34:55 -0300

    add Cart Routers

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const Router = express.Router();
 const path = require('path');
-const Mongodb = require('./config/mongodb');
-const signale = require('signale');
+// const Mongodb = require('./config/mongodb');
+// const signale = require('signale');
 
 const authRouters = require('./modules/auth/routers/authRouters');
 const userRouters = require('./modules/user/routers/userRouters'); 
 const sellerRouters = require('./modules/seller/routers/sellerRouters');
 const productRouters = require('./modules/product/routers/productRouters');
+//const categoryRouters = require('./modules/category/routers/categoryRouters');
+const cartRouters = require('./modules/cart/routers/cartRouters');
 
 /*
 const mongodb = Mongodb.getDb().then(function (db) {
@@ -23,5 +25,7 @@ Router.use("/auth", authRouters);
 Router.use("/users", userRouters);
 Router.use("/sellers", sellerRouters);
 Router.use("/products", productRouters);
+//Router.use("/categories", categoryRouters);
+Router.use("/cart", cartRouters);
 
 module.exports = Router;
