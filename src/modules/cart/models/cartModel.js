9cebf9da5539789dd1a2624f7051c78ebf7d18ec
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-28 23:24:47 -0300

    Update cartModel.js

--- src/modules/cart/models/cartModel.js
+++ src/modules/cart/models/cartModel.js
@@ -1,20 +1,19 @@
 const knex = require('../../../config/database');
 const Model = require('../../../repositories/models/model');
 const { v4: uuidv4 } = require('uuid');
-const seller = require('../../seller/models/sellerModel');
 
 class Cart extends Model{
-    async create(data){
+    async create({data}){
         try {
-			const sellerWasRegistered = await knex('sellers').where('ownerId', data.ownerId).select('id');
-			if(sellerWasRegistered.length > 0){
+			const cartWasRegistered = await knex('cart').where('ownerId', data.ownerId).select('id');
+			if(cartWasRegistered.length > 0){
 				const id = uuidv4();
 				delete data.ownerId;
-				const product = await knex('products').insert({...data, id, sellerId: sellerWasRegistered[0].id}).then(() => {return knex ('products').where('id', id).select('name')});
+				const product = await knex('products').insert({...data, id, sellerId: cartWasRegistered[0].id}).then(() => {return knex ('products').where('id', id).select('name')});
 				if(product.length > 0){
 					return {status: true, data: product[0]};
 				}else{
-					return {status: false, message: 'Produto não foi cadastrado'};
+					return {status: false, message: 'Carrinho não foi cadastrado'};
 				}
 			}else{
 				return {status: false, message: 'Loja não cadastrada', field: 'ownerId'};
