4b58d707e6f979c1f442f5f44277ce0ff82a8892
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-01-30 23:25:04 -0300

    cart add routers

---
+++ src/modules/cart/routers/cartRouters.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const AuthenticateToken = require('../../../middleware/authenticateToken');
+const CartController = require('../controllers/cartController');
+
+class userRouter {
+    static getRouter() {
+
+        const Router = express.Router();
+    
+        Router.post('/create', AuthenticateToken ,CartController.create);                                                                    //CREATE CART
+        Router.post('/toAdd', AuthenticateToken ,CartController.toAdd);                                                                      //ADD PRODUCT TO CART
+
+        return Router
+    }
+}
+
+module.exports = userRouter.getRouter();
